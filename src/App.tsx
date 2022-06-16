b993b9fe6f6cfdedb3f31cfc9940b703c0b80464
reza darzi <darzi.reza@gmail.com>
2022-06-16 19:36:31 +0430

    Refactor-> axios from result.component.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,30 @@ import React from "react";
 import "./App.css";
 import { Search } from "./components/search.component";
 import { Result } from "./components/result.component";
+import axios from "axios";
 
 const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 function App() {
   const [searchWord, setSearchWord] = React.useState<any>("");
+  const [searchResult, setSearchResult] = React.useState<any>();
+
+  React.useEffect(() => {
+    const baseURL = `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=${ACCESS_KEY}`;
+    axios
+      .get(baseURL)
+      .then((response) => setSearchResult(response.data.results))
+      .catch((error) => console.error(error.message));
+  }, [searchWord]);
 
   return (
     <div className="App">
       <Search setSearchWord={setSearchWord} />
-      <Result searchWord={searchWord} ACCESS_KEY={ACCESS_KEY} />
+      <div>
+        <h2>{searchWord}</h2>
+        {searchWord ? <hr></hr> : ""}
+      </div>
+      <Result searchResult={searchResult} />
     </div>
   );
 }
--- src/components/result.component.tsx
+++ src/components/result.component.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 import "./result.component.css";
-import axios from "axios";
 
-export const Result: React.FunctionComponent<any> = ({
-  searchWord,
-  ACCESS_KEY,
-}: any) => {
-  const [searchResult, setSearchResult] = React.useState<any>();
-  React.useEffect(() => {
-    const baseURL = `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=${ACCESS_KEY}`;
-    axios
-      .get(baseURL)
-      .then((response) => setSearchResult(response.data.results))
-      .catch((error) => console.error(error.message));
-  }, [searchWord]);
-
+export const Result: React.FunctionComponent<any> = ({ searchResult }: any) => {
   return (
     <div>
-      <h2>{searchWord}</h2>
       <div className="container">
         {searchResult?.map((result: any) => {
           return (
